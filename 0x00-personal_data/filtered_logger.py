1a52bc17a0aacd5862e8ef30f3590040033e8d71
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-16 16:59:52 +0100

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -70,3 +72,18 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connects to MySQL database and returns the connector"""
+    db_username = os.getenv('PERSONAL_DATA_DB_USERNAME')
+    db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
+    db_host = os.getenv('PERSONAL_DATA_DB_HOST')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    connection = mysql.connector.connect(user=db_username,
+                                         password=db_password,
+                                         host=db_host,
+                                         database=db_name)
+
+    return connection
