22a04e7276d611fd078bdd3cabbee8361baf4e50
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-16 16:09:55 +0100

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -65,7 +65,7 @@ def get_logger() -> logging.Logger:
     handler = logging.StreamHandler()
     formatter = RedactingFormatter(PII_FIELDS)
     handler.setFormatter(formatter)
-    
+
     logger.addHandler(handler)
 
     return logger
