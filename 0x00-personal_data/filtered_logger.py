a9ab9d79a2f102c956d9beb9f8bd867cd9b02734
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-16 17:34:56 +0100

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -87,3 +87,23 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                                          database=db_name)
 
     return connection
+
+
+def main():
+    """main entry"""
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = cursor.column_names
+
+    for row in cursor:
+        message = "".join("{}={};".format(i, v) for i, v in zip(fields, row))
+        logger.info(message.strip())
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
