b38a025b829333e3f21676cce9bf179c95e5e7c4
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-16 17:44:45 +0100

    Password Encryption

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines a function to encrpyt password"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """Returns a salted, hashed password"""
+    password_bytes = password.encode()
+    salt = bcrypt.gensalt()
+
+    return bcrypt.hashpw(password_bytes, salt)
