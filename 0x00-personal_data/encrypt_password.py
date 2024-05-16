4cc9fd2858dd3aaca562b7b339d6e743b3868b81
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-16 17:59:38 +0100

    Password Validation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
 
     return bcrypt.hashpw(password_bytes, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates password"""
+    password_bytes = password.encode()
+
+    return bcrypt.checkpw(password_bytes, hashed_password)
