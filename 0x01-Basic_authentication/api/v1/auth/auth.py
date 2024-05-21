022140a5b3462af917c4daf1b3317c5e868c16b9
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-21 01:55:57 +0100

    Auth Class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,7 @@ class Auth:
 
         Args:
           - path (str): The path to check
-          - excluded_paths (List[str]): List of paths that 
+          - excluded_paths (List[str]): List of paths that
             are excluded from authentication
 
         Return:
