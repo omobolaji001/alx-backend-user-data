00b488d6c1d2d53be12ef3ad5c2046764867f2ca
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-24 00:32:01 +0100

    Session Authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defines a class to represent the session authentication"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Represents Session Authentication"""
+    pass
