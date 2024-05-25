559ae9ed044b8bd94fbd622f7242f1a5fe0e0d8f
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-25 18:05:11 +0100

    New view for session authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Defines Session Auth view"""
+from api.v1.views import app_views
+from flask import abort, request, jsonify
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+def auth_login():
+    """Handles user login
+
+    Return:
+            Dictionary representation of user,  otherwise error message
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == '':
+        return jsonify({'error': 'email is missing'}), 400
+    if password is None or password == '':
+        return jsonify({'error': 'password is missing'}), 400
+
+    users = User.search({'email': email})
+    if users is None or users == []:
+        return jsonify({'error': 'no user found for this email'}), 404
+
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            response.set_cookie(session_name, session_id)
+
+            return response
+    return jsonify({'error': 'wrong password'}), 401
