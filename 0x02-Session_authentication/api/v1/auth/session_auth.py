95583fe6d37dddb91a9e07bcac951a20bebfaf4e
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-25 09:12:13 +0100

    Identify User with Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Defines a class to represent the session authentication"""
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -33,3 +34,11 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on cookie value"""
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+
+        user = User.get(user_id)
+        return user
