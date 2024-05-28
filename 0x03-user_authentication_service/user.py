570eac8040f5af590891445700ac891ebe74ac4d
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-28 15:53:18 +0100

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """Defines a User model with SQLAlchemy
 for a database named users.
 """
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
@@ -11,6 +11,7 @@ Base = declarative_base()
 class User(Base):
     """User model"""
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
