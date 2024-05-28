576f8e80b1c844885c0c878f74730f8d91977239
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-29 00:32:58 +0100

    Password hashing

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+"""Defines _hash_password
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password string and returns it in bytes form
+    """
+    encoded_password = password.encode('utf-8')
+    return bcrypt.hashpw(encoded_password, bcrypt.gensalt())
