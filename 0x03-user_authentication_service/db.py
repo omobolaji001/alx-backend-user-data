16de7f801b7ed90d1562842f951353e47ec04996
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-28 17:42:25 +0100

    Find User by attribute

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +40,17 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs: dict) -> str:
+        """Finds user by key-word arugument
+        """
+        all_users = self._session.query(User).all()
+
+        for key, value in kwargs.items():
+            if key not in User.__dict__:
+                raise InvalidRequestError
+
+            for user in all_users:
+                if getattr(user, key) == value:
+                    return user
+        raise NoResultFound
