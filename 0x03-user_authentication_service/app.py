6f6e3b318049b47d58cdb50443948b4efdebad84
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-30 12:44:59 +0100

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,7 +61,7 @@ def logout():
     if session_id is None or user is None:
         abort(403)
     AUTH.destroy_session(user.id)
-    return redirect(url_for("/"))
+    return redirect("/", 301)
 
 
 if __name__ == "__main__":
