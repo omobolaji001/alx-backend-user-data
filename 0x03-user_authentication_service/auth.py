218814a04363e2e2a24eeacc694bfcf55b421fc3
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-30 10:39:55 +0100

    Get user by Session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,11 +61,11 @@ class Auth:
         except Exception:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Retrieves user based on session_id
         """
         try:
-            user = self._db.find_user_by(session_id=session_d)
+            user = self._db.find_user_by(session_id=session_id)
             return user
         except Exception:
             return None
--- 0x03-user_authentication_service/main_7.py
+++ 0x03-user_authentication_service/main_7.py
@@ -8,7 +8,9 @@ email = 'bob@bob.com'
 password = 'MyPwdOfBob'
 auth = Auth()
 
-auth.register_user(email, password)
+print(auth.register_user(email, password))
 
-print(auth.create_session(email))
+session_id = auth.create_session(email)
 print(auth.create_session("unknown@email.com"))
+print(session_id)
+print(auth.get_user_from_session_id(session_id))
