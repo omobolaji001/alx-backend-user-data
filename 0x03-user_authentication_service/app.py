e4ba16d458fc7cba9fbac748d2c94a836d0e6ad3
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-30 13:22:51 +0100

    user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,5 +64,17 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """user profile
+    """
+    session_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": f"{user.email}"}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
